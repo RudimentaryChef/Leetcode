9c71f6e6b9ee13ffe78aec936bca5b002f159100
RudimentaryChef <akrish6@gatech.edu>
2023-10-21 13:49:10 -0400

    number of subarrays product less than k function added

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+//Welcome to my scratch pad for random programming problems I thought sounded fun or cool!
+//I usually warm up or do these when I'm a bit bored
 public class Main {
     public static void main(String[] args) {
-        System.out.println(longestBinary("1101100111"));
+        //System.out.println(longestBinary("1101100111"));
+
     }
     public static int longestBinary(String s){
         //Problem:
@@ -28,6 +29,30 @@ public class Main {
 
 
         return answer;
+    }
+    public static int numSubarrayProductLessThanK(int[] nums, int k){
+        // return the number of subarrays where the product of all the elements in the subarray is strictly less than k
+        //An important math trick is to note that the number of sub arrays that end at right = length of subarray
+        int answer = 0;
+        int curr = 1;
+        int left = 0;
+        if(k <= 1){
+            return 0;
+        }
+        for(int right = 0;right<nums.length;right++){
+            curr = curr * nums[right];
+
+            if(curr >= k){
+                while(curr >= k){
+                    curr = curr/nums[left];
+                    left = left + 1;
+                }
+            }
+            answer = answer + right - left + 1;
+
+        }
+
+        return 0;
     }
 
 }
