f7dc3ffb40c4b4643adaf31a03ff8d30cd7f227c
RudimentaryChef <akrish6@gatech.edu>
2023-10-20 20:35:32 -0400

    added problem statement to longestBinary

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,10 @@ public class Main {
         System.out.println(longestBinary("1101100111"));
     }
     public static int longestBinary(String s){
-
+        //Problem:
+        //You are given a binary string s (a string containing only "0" and "1").
+        // You may choose up to one "0" and flip it to a "1".
+        // What is the length of the longest substring achievable that contains only "1"?
         int answer = 0;
         int left = 0;
         int zero_count = 0;
