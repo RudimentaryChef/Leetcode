d9e90863da30a5cf3a407e2eed61e68aadfcb282
RudimentaryChef <akrish6@gatech.edu>
2023-10-23 13:04:30 -0400

    adds find kth element of linked list

--- src/Main.java
+++ src/Main.java
@@ -246,6 +246,21 @@ public class Main {
         }
         return false;
     }
+    public static ListNode FindKth(ListNode head, int k){
+        ListNode slow = head;
+        ListNode fast = head;
+        for(int i = 0;i<k;i++){
+            fast = fast.next;
+        }
+        while(fast!= null){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+
+
+
+    }
 
 
 
